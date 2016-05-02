b48ecde64b98e626797b87b743584cb3b6906dcc
marianbuchly <marianbuchly@gmail.com>
2016-05-02 12:01:22 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import Gamemodel from './Models/Gamemodel'
 
 class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      games: []
+    };
+  }
+
+  componentDidMount(){
+    this.games = new GameModel();
+    this.games.subscribe(this.updateList.bind(this));
+  }
+
+
+updateList(){
+  this.setState({
+    games:this.games.resources
+  });
+}
+
+  containerStyles(){
+    return {
+      width: "500px",
+      height: "500px",
+      margin: "auto"
+    };
+  }
+
+  headerStyle(){
+    return {
+        textAlign: "center"
+    };
+
+  }
     render() {
         return (
-            <h1>Hello World!</h1>
+            <div style={this.containerStyles()}>
+              <h1>Rock Paper Siccors</h1>
+              </div>
         );
     }
 }
